719b7becb57102509b417cea9d1a4ffe1bc3ed2a
Prawesh kumar <praweshwebs@gmail.com>
2024-03-28 10:33:30 +0530

    new

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slickmenu').slick({
+    variableWidth:true,
+    // arrows: false,
+    prevArrow:'<button type="button" class="slick-prev"><i class="bi bi-arrow-left"></i></button>',
+    nextArrow:'<button type="button" class="slick-next">R</button>',
+
+  });
+})
